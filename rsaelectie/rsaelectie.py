733bafe465bbb672087b65fb946e792b5926737e
Matus Stas <matus.stas@itaps.sk>
2022-01-29 13:26:48 +0100

    EV-114 Add async and await

--- rsaelectie/rsaelectie.py
+++ rsaelectie/rsaelectie.py
@@ -16,7 +16,7 @@ class Data(BaseModel):
     candidates_ids: List[str] = []
 
 
-def validate_to_be_encrypted_data(data: Data):
+async def validate_to_be_encrypted_data(data: Data):
     if len(list(data.keys())) != 4:
         raise Exception("Incorrect number of keys")
 
@@ -34,12 +34,12 @@ def validate_to_be_encrypted_data(data: Data):
         raise Exception("Incorrect format for key 'candidates_ids'")
 
 
-def validate_to_be_decrypted_data(data: str):
+async def validate_to_be_decrypted_data(data: str):
     if type(data) != str:
         raise Exception("Incorrect type for parameter 'data'")
 
 
-def get_rsa_key_pair(key_length: int):
+async def get_rsa_key_pair(key_length: int):
     private_key = RSA.generate(key_length)
     public_key = private_key.publickey()
 
@@ -49,27 +49,27 @@ def get_rsa_key_pair(key_length: int):
     return private_key_pem, public_key_pem
 
 
-def encrypt_message(message, public_key):
+async def encrypt_message(message, public_key):
     encryptor = PKCS1_OAEP.new(public_key)
     encrypted = encryptor.encrypt(bytes(message, encoding="utf-8"))
     return base64.b64encode(encrypted)
 
 
-def decrypt_message(base64_encoded_message, private_key):
+async def decrypt_message(base64_encoded_message, private_key):
     decrypted = base64.b64decode(base64_encoded_message)
     decryptor = PKCS1_OAEP.new(private_key)
     return decryptor.decrypt(decrypted)
 
 
-def encrypt_vote(public_key_pem: str, data: Data):
+async def encrypt_vote(public_key_pem: str, data: Data):
     try:
-        validate_to_be_encrypted_data(data)
+        await validate_to_be_encrypted_data(data)
         data = json.dumps(data)
         try: 
             public_key_pem = public_key_pem.encode("utf-8")
             public_key = RSA.import_key(public_key_pem)
 
-            encrypted_data = encrypt_message(data, public_key)
+            encrypted_data = await encrypt_message(data, public_key)
             encrypted_data = encrypted_data.decode("utf-8")
             return encrypted_data
         except:
@@ -78,14 +78,14 @@ def encrypt_vote(public_key_pem: str, data: Data):
         traceback.print_exc()
 
 
-def decrypt_vote(private_key_pem: str, data: str):
+async def decrypt_vote(private_key_pem: str, data: str):
     try:
-        validate_to_be_decrypted_data(data)
+        await validate_to_be_decrypted_data(data)
         try: 
             private_key_pem = private_key_pem.encode("utf-8")
             private_key = RSA.import_key(private_key_pem)
 
-            decrypted_data = decrypt_message(data, private_key)
+            decrypted_data = await decrypt_message(data, private_key)
             decrypted_data = decrypted_data.decode("utf-8")
             decrypted_data = json.loads(decrypted_data)
             return decrypted_data
