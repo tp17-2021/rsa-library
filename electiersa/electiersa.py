8b6fcf72f326c613ca23fc96306c8044d285c36f
Matus Stas <matus.stas@itaps.sk>
2022-02-07 12:37:58 +0100

    EV-124 Change to AES-RSA encryption

--- electiersa/electiersa.py
+++ electiersa/electiersa.py
@@ -5,48 +5,55 @@ import traceback
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-# RSA
-# data = around 150 bytes
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
-# 2048 bits = 256 bytes -> 214 bytes is ok (42 padding) cca 70% utilized
-# 4096 bits = 512 bytes -> 470 bytes is ok (42 padding) cca 32% utilized
+import asyncio
 
 from typing import List
 from pydantic import BaseModel
 
-KEY_LENGTH = 4096
+KEY_LENGTH = 2048
+AES_KEY_LENGTH = 32
 
 # for this phase election_id = "election_id"
-class Data(BaseModel):
+class Vote(BaseModel):
     token: str = None
     party_id: int
     election_id: str
     candidates_ids: List[int] = []
 
 
-async def validate_to_be_encrypted_data(data: Data):
-    if len(list(data.keys())) != 4:
+async def validate_vote(vote: Vote):
+    if len(list(vote.keys())) != 4:
         raise Exception("Incorrect number of keys")
 
-    if "token" not in data or type(data["token"]) != str:
+    if "token" not in vote or type(vote["token"]) != str:
         raise Exception("Incorrect format for key 'token'")
 
-    if "party_id" not in data or type(data["party_id"]) != int:
+    if "party_id" not in vote or type(vote["party_id"]) != int:
         raise Exception("Incorrect format for key 'party_id'")
 
 
-    if "election_id" not in data or type(data["election_id"]) != str:
+    if "election_id" not in vote or type(vote["election_id"]) != str:
         raise Exception("Incorrect format for key 'election_id'")
 
-    if "candidates_ids" not in data or type(data["candidates_ids"]) != list or \
-            not all([type(candidate_id) == int for candidate_id in data["candidates_ids"]]):
+    if "candidates_ids" not in vote or type(vote["candidates_ids"]) != list or \
+            not all([type(candidate_id) == int for candidate_id in vote["candidates_ids"]]):
         raise Exception("Incorrect format for key 'candidates_ids'")
 
 
-async def validate_to_be_decrypted_data(data: str):
-    if type(data) != str:
+async def validate_encrypted_vote(vote: str):
+    if type(vote) != str:
         raise Exception("Incorrect type for parameter 'data'")
 
+
+async def from_bytes_to_string(encrypted_bytes):
+    return base64.b64encode(encrypted_bytes).decode("utf-8")
+
+async def from_string_to_bytes(string):
+    return base64.b64decode(string.encode("utf-8"))
+     
 
 async def get_rsa_key_pair():
     """
@@ -57,87 +64,136 @@ async def get_rsa_key_pair():
 
     private_key_pem = private_key.exportKey()
     public_key_pem = public_key.exportKey()
+
+    private_key_pem = private_key_pem.decode("utf-8")
+    public_key_pem = public_key_pem.decode("utf-8")
     
     return private_key_pem, public_key_pem
 
 
-async def encrypt_message(message, public_key):
-    encryptor = PKCS1_OAEP.new(public_key)
-    encrypted = encryptor.encrypt(bytes(message, encoding="utf-8"))
-    return base64.b64encode(encrypted)
+async def get_aes_key():
+    global AES_KEY_LENGTH
+    aes_key = get_random_bytes(AES_KEY_LENGTH)
+    aes_key = await from_bytes_to_string(aes_key)   
+    return aes_key
 
 
-async def decrypt_message(base64_encoded_message, private_key):
-    decrypted = base64.b64decode(base64_encoded_message)
-    decryptor = PKCS1_OAEP.new(private_key)
-    return decryptor.decrypt(decrypted)
+async def encrypt_message_with_aes_key(message: str, aes_key: str):
+    message = message.encode("utf-8")
+
+    aes_key = await from_string_to_bytes(aes_key)
+    aes_encryptor = AES.new(aes_key, AES.MODE_EAX)
+
+    encrypted_message, tag = aes_encryptor.encrypt_and_digest(message)
+    encrypted_message = await from_bytes_to_string(encrypted_message)
+    tag = await from_bytes_to_string(tag)
+    nonce = await from_bytes_to_string(aes_encryptor.nonce)
+
+    return encrypted_message, tag, nonce
 
 
-async def encrypt_vote(public_key_pem: str, data: Data):
-    try:
-        await validate_to_be_encrypted_data(data)
-        data = json.dumps(data)
-        try: 
-            public_key_pem = public_key_pem.encode("utf-8")
-            public_key = RSA.import_key(public_key_pem)
+async def encrypt_aes_key_with_rsa_pub_key(aes_key: str, rsa_public_key_pem: str):
+    aes_key = await from_string_to_bytes(aes_key)
 
-            encrypted_data = await encrypt_message(data, public_key)
-            encrypted_data = encrypted_data.decode("utf-8")
-            return encrypted_data
-        except:
-            traceback.print_exc()
+    rsa_public_key_pem = rsa_public_key_pem.encode("utf-8")
+    rsa_public_key = RSA.import_key(rsa_public_key_pem)
+    rsa_encryptor = PKCS1_OAEP.new(rsa_public_key)
+
+    encrypted_aes_key = rsa_encryptor.encrypt(aes_key)
+    encrypted_aes_key = await from_bytes_to_string(encrypted_aes_key)
+    
+    return encrypted_aes_key
+
+
+async def decrypt_aes_key_with_rsa_priv_key(encrypted_aes_key: str, rsa_private_key_pem: str):
+    encrypted_aes_key = await from_string_to_bytes(encrypted_aes_key)
+
+    rsa_private_key_pem = rsa_private_key_pem.encode("utf-8")
+    rsa_private_key = RSA.import_key(rsa_private_key_pem)
+    rsa_decryptor = PKCS1_OAEP.new(rsa_private_key)
+
+    decrypted_aes_key = rsa_decryptor.decrypt(encrypted_aes_key)
+    decrypted_aes_key = await from_bytes_to_string(decrypted_aes_key)
+
+    return decrypted_aes_key
+
+
+async def decrypt_message_with_aes_key(encrypted_message: str, tag: str, nonce: str, aes_key: str):
+    encrypted_message = await from_string_to_bytes(encrypted_message)
+    tag = await from_string_to_bytes(tag)
+    nonce = await from_string_to_bytes(nonce)
+    aes_key = await from_string_to_bytes(aes_key)
+
+    aes_decryptor = AES.new(aes_key, AES.MODE_EAX, nonce)
+    decrypted_message = aes_decryptor.decrypt_and_verify(encrypted_message, tag)
+    decrypted_message = decrypted_message.decode("utf-8")
+
+    return decrypted_message
+
+
+async def encrypt_vote(vote: Vote, aes_key: str, rsa_public_key_pem: str):
+    try: 
+        await validate_vote(vote)
+        message = json.dumps(vote)
+
+        encrypted_message, tag, nonce = await encrypt_message_with_aes_key(message, aes_key)
+        encrypted_aes_key = await encrypt_aes_key_with_rsa_pub_key(aes_key, rsa_public_key_pem)
+        return encrypted_message, tag, nonce, encrypted_aes_key
     except:
         traceback.print_exc()
 
 
-async def decrypt_vote(private_key_pem: str, data: str):
+async def decrypt_vote(encrypted_vote: str, tag: str, nonce: str, encrypted_aes_key: str, rsa_private_key_pem: str):
     try:
-        await validate_to_be_decrypted_data(data)
-        try: 
-            private_key_pem = private_key_pem.encode("utf-8")
-            private_key = RSA.import_key(private_key_pem)
+        await validate_encrypted_vote(encrypted_vote)
 
-            decrypted_data = await decrypt_message(data, private_key)
-            decrypted_data = decrypted_data.decode("utf-8")
-            decrypted_data = json.loads(decrypted_data)
-            return decrypted_data
-        except:
-            traceback.print_exc()
+        decrypted_aes_key = await decrypt_aes_key_with_rsa_priv_key(encrypted_aes_key, rsa_private_key_pem)
+        decrypted_message = await decrypt_message_with_aes_key(encrypted_vote, tag, nonce, decrypted_aes_key)
+        
+        vote = json.loads(decrypted_message)
+        return vote
     except:
         traceback.print_exc()
 
 
-# async def tmp():
-#     private_key_pem, public_key_pem = await get_rsa_key_pair()
-#     print(private_key_pem)
-#     print(public_key_pem)
+async def example():
+    # CLIENT
+    vote = {
+        "token": "A"*64,
+        "election_id": "election_id",
+        "party_id": 10000,
+        "candidates_ids": [
+            10000,
+            10000,
+            10000,
+            10000,
+            10000
+        ]
+    }
 
-#     private_key_pem = private_key_pem.decode("utf-8")
-#     public_key_pem = public_key_pem.decode("utf-8")
+    aes_key = await get_aes_key()
+    rsa_private_key_pem, rsa_public_key_pem = await get_rsa_key_pair()
+    encrypted_vote, tag, nonce, encrypted_aes_key = await encrypt_vote(vote, aes_key, rsa_public_key_pem)
 
+    # SERVER
+    decrypted_vote = await decrypt_vote(encrypted_vote, tag, nonce, encrypted_aes_key, rsa_private_key_pem)
+    print(decrypted_vote)
 
-#     data = {
-#         "token": "A"*64,
-#         "election_id": "election_id",
-#         "party_id": 10000,
-#         "candidates_ids": [
-#             10000,
-#             10000,
-#             10000,
-#             10000,
-#             10000
-#         ]
-#     }
-
-#     print(data)
-
-#     data_e = encrypt_vote(public_key_pem, data)
-#     print(data_e)
-
-#     print("---")
-#     data_d = decrypt_vote(private_key_pem, data_e)
-#     print(data_d)
+asyncio.run(example())
 
 
-# import asyncio
-# asyncio.run(tmp())
+# CLIENT
+# vote
+# aes key
+# rsa public key
+
+# SERVER
+# rsa private key
+
+# https://wizardforcel.gitbooks.io/practical-cryptography-for-developers-book/content/asymmetric-key-ciphers/ecc-encryption-decryption.html
+# https://www.youtube.com/watch?v=p3jraFbfnHw&ab_channel=MisterArk
+
+# RSA
+# data = around 150 bytes
+# 2048 bits = 256 bytes -> 214 bytes is ok (42 padding) cca 70% utilized
+# 4096 bits = 512 bytes -> 470 bytes is ok (42 padding) cca 32% utilized
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="electiersa",
-    version="0.0.4",
+    version="0.0.6",
     description="Adapted RSA library for university project called Electie",
     author="tim172021@googlegroups.com",
     license="MIT",
